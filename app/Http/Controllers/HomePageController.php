a8fe7544662ae8a264664e0a9062100754007cbb
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-12 15:51:17 +0200

    feat: add where condition in HomePageController to display only today trains

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Train;
+use Carbon\Carbon;
 
 class HomePageController extends Controller
 {
     public function index(){
-        $trains = Train::all();
+        $today = Carbon::now(); 
+
+        $trains = Train::where('departure_date', '>=', $today)->get();
+
         return view('home', compact('trains'));
     }
 }
